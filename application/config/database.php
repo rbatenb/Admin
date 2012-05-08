5e44628938dd60a525ef0dea9e69ce7a70fede18
Stagiair <stagiair@orangetalent.nl>
2012-05-08 10:55:34 +0200

    Added DB details

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'deb37392_cms';
+$db['default']['password'] = 'py1IciqK';
+$db['default']['database'] = 'deb37392_cms';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
