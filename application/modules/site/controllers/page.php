137914e1c4f86cca23fa1e8076c419e9d93fcf09
Stagiair <stagiair@orangetalent.nl>
2012-05-08 12:29:39 +0200

    Added page module

---
+++ application/modules/site/controllers/page.php
@@ -0,0 +1,12 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Page extends MX_Controller 
+{
+	public function index()
+	{
+		echo 'PAGE';
+	}
+}
+
+/* End of file page.php */
+/* Location: ./application/modules/site/controllers/page.php */
